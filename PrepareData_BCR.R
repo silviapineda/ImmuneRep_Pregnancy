a092ffb960ea2e8748e6011a9834f22d50f2b24a
silviapineda <silviapinedasanjuan@gmail.com>
2018-12-20 12:56:21 +0100

    Diversity measures

--- PrepareData_BCR.R
+++ PrepareData_BCR.R
@@ -86,6 +86,37 @@ id<-match(rownames(reads_clones_igh_SHM),cdr3_length$Group.1)
 reads_clones_igh_SHM_cdr3length<-cbind(reads_clones_igh_SHM,cdr3_length$x[id])
 colnames(reads_clones_igh_SHM_cdr3length)[10]<-c("mean_CDR3_length")
 
-summary_data<-reads_clones_igh_SHM_cdr3length
+summary_data<-data.frame(reads_clones_igh_SHM_cdr3length)
+
+###Introduce a variable to design mother and fetal
+info_samples<-read.csv("Data/NormalSamplesScottBoydStanford9_13_18.csv")
+summary_data$pairs<-info_samples$Pairs
+summary_data$sample<-gsub("\\*", "", substr(summary_data$pairs, 1, 1))
+summary_data$sample<-ifelse(summary_data$sample=="M","Mother","Fetus")
+summary_data$sample<-factor(summary_data$sample)
+summary_data$sample<-relevel(summary_data$sample, ref = "Mother")
+
+## Diversity measures
+sample<-rownames(summary_data)
+entropy<-rep(NA,length(sample))
+simpson<-rep(NA,length(sample))
+for (i in 1:length(sample)){
+  print(i)
+  data_sample_unique<-data_qc[which(data_qc$sample_label==sample[i]),]
+  clones_sample<-data_sample_unique[,"V_J_lenghCDR3_Clone_igh"]
+  #To write file to run with Recon
+  #write.delim(data.frame(table(table(clones_sample))),file=paste("clones_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  fi<-as.numeric(table(clones_sample))/length(clones_sample)
+  hi<-fi*log2(fi)
+  entropy[i]=-sum(hi) #entropy(table(clones_specimen)) returns the same result but by default is the natural logarithm (log)
+  simpson[i]=sum(fi*fi) 
+}
+entropy_norm<-entropy/max(entropy,na.rm = T)
+clonality<-(1-entropy_norm)
+names(clonality)<-sample
+diversity<-cbind(clonality,entropy,simpson)
+
+summary_data<-cbind(summary_data,diversity)
+
 save(data_qc,summary_data,file="Data/BCR_data_summary.RData")
 
