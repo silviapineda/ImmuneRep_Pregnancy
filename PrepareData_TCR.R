6c95b9c4e705a93bdc244dead780b4f06eb80261
Brian Le <Brian.Le@ucsf.edu>
2018-12-21 10:43:23 -0800

    TCR analysis
    Reproduced preliminary analysis of BCR data for TCR data

---
+++ PrepareData_TCR.R
@@ -0,0 +1,133 @@
+rm(list = ls(all = TRUE))
+x<-date()
+print(x)
+###########################################################################################
+### PROJECT: Immune Repertoire. Analysis T cells antibodies for pregnancy
+###
+### CITATION: Modified from Silvia's PrepareData_BCR.R
+###
+### PROCESS: 
+###           
+### DESCRIP: 
+###         
+###
+### Author: Brian Le
+### Date: December, 2018
+############################################################################################
+
+#Run once to combine data files together and save as R workspace
+
+##Read all the files and save into and Rdata all together
+directory <- "TCR/"
+
+files <- list.files(directory)
+
+data <- c()
+for(i in files) {
+  cat(i, "\n")
+  t <- read.delim(paste(directory, i, sep = ""))
+  data <- rbind(data, t)
+}
+save(data, file=paste(directory, 'TCR_data.RData', sep = ""))
+
+##############################
+###Some Quality Control
+#############################
+directory <- "TCR/"
+
+load(paste(directory, 'TCR_data.RData', sep = ""))
+##75% of v-scores are below 124, discarding all below 200 results in no leftover data
+##so keep all for now
+data_qc<-data[which(data$v_score>=0),]
+##Discard the non-functional sequences
+data_qc<-data_qc[which(data_qc$productive=="t"),]
+
+##Extract the gene from the segment with the allele
+data_qc$v_gene <- gsub("\\*", "", substr(data_qc$v_segment, 1, 8))
+data_qc$j_gene <- gsub("\\*", "", substr(data_qc$j_segment, 1, 5))
+data_qc$d_gene <- gsub("\\*", "", substr(data_qc$d_segment, 1, 8))
+
+###Extract the CDR3 region
+data_qc$cdr3_seq <- gsub(" ","", data_qc$cdr3_seq_nt_q)
+###Extract the isotype
+#isosubtype column doesn't exist in this data
+#data_qc$isotype <- substr(data_qc$isosubtype, 1, 4)
+
+##Count the somatic hypermutations
+data_qc$Vlength<-nchar(as.character(data_qc$v_sequence))
+v_sequence = as.character(data_qc$v_sequence)
+data_qc$SHM<-sapply(regmatches(v_sequence, gregexpr("[A-Z]", v_sequence, perl=TRUE)), length)
+data_qc$SHM_freq<-data_qc$SHM/data_qc$Vlength
+
+##count the CDR3 length
+data_qc$CDR3_length<-nchar(as.character(data_qc$cdr3_seq))
+
+##count the read length
+data_qc$read_length<-nchar(as.character(data_qc$trimmed_sequence))
+
+##Read counts and clones per sample and data point
+read_count <- table(data_qc$sample_label)
+#no isosubtypes in TCR data!
+#read_count_isotype <- table(data_qc$sample_label, data_qc$isotype)
+#colnames(read_count_isotype)[1] = "UNMAPPED"
+#reads <- cbind(read_count,read_count_isotype)
+reads <- cbind(read_count)
+
+
+##Count number of clones per sample 
+data_qc$V_J_lenghCDR3_Clone_igh = paste(data_qc$v_gene, data_qc$j_gene, nchar(data_qc$cdr3_seq),data_qc$igh_clone_id,sep="_")
+read_count_ighClones<- unique(data_qc[,c("sample_label","V_J_lenghCDR3_Clone_igh")])
+clones_igh<-data.matrix(table(read_count_ighClones$sample_label))
+colnames(clones_igh)<-c("clones")
+
+reads_clones_igh<-cbind(reads,clones_igh)
+
+#####SHM
+sum_SHM_freq<-aggregate(data_qc$SHM_freq, by=list(data_qc$sample_label), FUN=sum)
+id<-match(rownames(reads_clones_igh),sum_SHM_freq$Group.1)
+colnames(sum_SHM_freq)[2]<-c("SHM")
+reads_clones_igh_SHM<-cbind(reads_clones_igh,sum_SHM_freq$SHM[id])
+#colnames(reads_clones_igh_SHM)[9]<-c("SHM")
+colnames(reads_clones_igh_SHM)[3]<-c("SHM")
+
+###length of CDR3
+cdr3_length<-aggregate(data_qc$CDR3_length,by=list(data_qc$sample_label), FUN=mean)
+id<-match(rownames(reads_clones_igh_SHM),cdr3_length$Group.1)
+reads_clones_igh_SHM_cdr3length<-cbind(reads_clones_igh_SHM,cdr3_length$x[id])
+#colnames(reads_clones_igh_SHM_cdr3length)[10]<-c("mean_CDR3_length")
+colnames(reads_clones_igh_SHM_cdr3length)[4]<-c("mean_CDR3_length")
+
+summary_data<-data.frame(reads_clones_igh_SHM_cdr3length)
+
+###Introduce a variable to design mother and fetal
+#info_samples<-read.csv("Data/NormalSamplesScottBoydStanford9_13_18.csv")
+summary_data$pairs<-row.names(summary_data)
+summary_data$sample<-gsub("\\*", "", substr(summary_data$pairs, 1, 1))
+summary_data$sample<-ifelse(summary_data$sample=="M","Mother","Fetus")
+summary_data$sample<-factor(summary_data$sample)
+summary_data$sample<-relevel(summary_data$sample, ref = "Mother")
+
+## Diversity measures
+sample<-rownames(summary_data)
+entropy<-rep(NA,length(sample))
+simpson<-rep(NA,length(sample))
+for (i in 1:length(sample)){
+  print(i)
+  data_sample_unique<-data_qc[which(data_qc$sample_label==sample[i]),]
+  clones_sample<-data_sample_unique[,"V_J_lenghCDR3_Clone_igh"]
+  #To write file to run with Recon
+  #write.delim(data.frame(table(table(clones_sample))),file=paste("clones_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  fi<-as.numeric(table(clones_sample))/length(clones_sample)
+  hi<-fi*log2(fi)
+  entropy[i]=-sum(hi) #entropy(table(clones_specimen)) returns the same result but by default is the natural logarithm (log)
+  simpson[i]=sum(fi*fi) 
+}
+entropy_norm<-entropy/max(entropy,na.rm = T)
+clonality<-(1-entropy_norm)
+names(clonality)<-sample
+diversity<-cbind(clonality,entropy,simpson)
+
+summary_data<-cbind(summary_data,diversity)
+
+save(data_qc,summary_data,file=paste(directory, "TCR_data_summary.RData", split = ""))
+
---
+++ SummaryPlotsTCR.R
@@ -0,0 +1,70 @@
+rm(list = ls(all = TRUE))
+x<-date()
+print(x)
+###########################################################################################
+### PROJECT: Immune Repertoire. Analysis T cells antibodies for pregnancy outcomes
+###
+### CITATION: Modified from Silvia's SummaryPlots.R
+###
+### PROCESS: 
+###           
+### DESCRIP: Analysis of the VDJ data
+###         
+###
+### Author: Brian Le
+### Date: December, 2018
+############################################################################################
+library("RColorBrewer")
+
+directory <- "TCR/"
+
+load(paste(directory, 'TCR_data_summary.RData', sep = ""))
+
+plot_directory <- "TCR/Results/"
+dir.create(plot_directory)
+
+####Summary plots
+brewer.pal(n = 3, name = "Accent")
+COLOR=c("#BEAED4","#7FC97F")
+
+tiff(paste(plot_directory, "barplot_reads.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$read_count,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="Number of Reads",xlab = "Samples", ylab = "Reads",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
+
+tiff(paste(plot_directory, "barplot_clones.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$clones,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="Number of Clones",xlab = "Samples", ylab = "Clones",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
+
+tiff(paste(plot_directory, "barplot_SHM.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$SHM,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="SHM",xlab = "Samples", ylab = "SHM",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
+
+tiff(paste(plot_directory, "barplot_cdr3length.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$mean_CDR3_length,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="mean_CDR3_length",xlab = "Samples", ylab = "mean_CDR3_length",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
+
+tiff(paste(plot_directory, "entropy.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$entropy,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="entropy",xlab = "Samples", ylab = "entropy",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
+
+tiff(paste(plot_directory, "simpson.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$simpson,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="simpson",xlab = "Samples", ylab = "simpson",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
+
+tiff(paste(plot_directory, "clonality.tiff", split = ""),res=300,w=4000,h=2000)
+barplot(summary_data$clonality,col=COLOR[summary_data$sample],
+        names.arg = summary_data$pairs,main="clonality",xlab = "Samples", ylab = "clonality",las=2)
+legend("topright", legend=levels(summary_data$sample),col=COLOR[factor(summary_data$sample)],pch=15, cex=0.8)
+dev.off()
