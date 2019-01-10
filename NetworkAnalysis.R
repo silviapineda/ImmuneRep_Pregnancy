f8fa81f95a4cc22e5d832e927e7780bb8056ee88
silviapineda <silviapinedasanjuan@gmail.com>
2019-01-10 10:31:19 +0100

    Added the Gini Index

--- NetworkAnalysis.R
+++ NetworkAnalysis.R
@@ -18,6 +18,7 @@ library(ggplot2)
 library("igraph")
 library("ineq")
 library("dplyr")
+library("RColorBrewer")
 
 setwd("/Users/Pinedasans/ImmuneRep_Pregnancy/")
 load("Data/BCR_data_summary.RData")
@@ -41,13 +42,16 @@ Obtain_vertex_edges<-function(data,isotype){
 }
 
 network_IGHM<-Obtain_vertex_edges(data_qc,"IGHM")
+network_IGHD<-Obtain_vertex_edges(data_qc,"IGHD")
+network_IGHG<-Obtain_vertex_edges(data_qc,"IGHG")
+network_IGHA<-Obtain_vertex_edges(data_qc,"IGHA")
 
 ##2.Apply the nucleotides-assembly-1.0.jar made by Mikel using the Network.sh 
 
 ##3.Plot the network
 sample<-unique(data_qc$sample_label)
 
-isotype="IGHM"
+isotype="IGHA"
 
 ##Mother
 sample_M<-sample[which(summary_data$sample=="Mother")] 
@@ -84,3 +88,63 @@ for(i in sample_F) {
   plot(net,vertex.label=NA,layout=layout_with_graphopt(net))
   dev.off()
 }
+
+
+##4. Obtain the vertex and cluster Gini index
+Obtain_gini_index<-function(data,isotype,summary_data){
+  data<-data[which(data$isotype==isotype),]
+  data$CloneId_CDR3<-paste0(data[,c("V_J_lenghCDR3_Clone_igh")],data[,c("cdr3_seq_aa_q")])
+  sample<-unique(data$sample_label)
+  
+  vertex_max<-NULL
+  vertex_gini<-NULL
+  cluster_max<-NULL
+  cluster_gini<-NULL
+  num_reads_max_cluster<-NULL
+  clusters<-NULL
+  j<-1
+  for (i in sample){
+    assign(paste0("edges",i),read.delim(paste0("Results/Network/",isotype,"/edges_",isotype,"_",i,".txt")))
+    assign(paste0("vertex",i),read.delim(paste0("Results/Network/",isotype,"/vertex_",isotype,"_",i,".txt")))
+    vertex_max[j]<-max(get(paste0("vertex",i))$Freq)
+    vertex_gini[j]<-Gini(get(paste0("vertex",i))$Freq)
+    cluster_max[j]<-max(table(get(paste0("edges",i))$V_J_lenghCDR3_Clone_igh))
+    clusters[j]<-sum(table(table(get(paste0("edges",i))$V_J_lenghCDR3_Clone_igh)))
+    num_reads_max_cluster[j]<-tail(table(table(get(paste0("edges",i))$V_J_lenghCDR3_Clone_igh)),1)
+    cluster_gini[j]<-Gini(table(get(paste0("edges",i))$V_J_lenghCDR3_Clone_igh))
+    j=j+1
+  }
+  clonal_expansion<-(num_reads_max_cluster/summary_data[,"IGHM"])*100
+  results<-cbind(cluster_gini,vertex_gini,vertex_max,cluster_max,num_reads_max_cluster,clusters,clonal_expansion)
+  
+  rownames(results)<-sample
+  
+  return(results)
+}
+
+###Plot Gini boxplot
+assign(paste0("cluster_gini_",isotype),data.frame(Obtain_gini_index(data_qc,isotype,summary_data)))
+
+brewer.pal(n = 3, name = "Accent")
+COLOR=c("#BEAED4","#7FC97F")
+
+tiff(paste0("Results/network_vertex_cluster_gini_",isotype,".tiff"),h=2000,w=2000,res=300)
+par(fig=c(0,0.8,0,0.8))
+plot(get(paste0("cluster_gini_",isotype))[,"cluster_gini"], 
+         get(paste0("cluster_gini_",isotype))[,"vertex_gini"],
+     col = COLOR,pch=20,ylab = "Gini (Vextex)",xlab = "Gini (Cluster)")
+legend("bottomright",legend=c("Maternal","Fetal"), 
+       col=COLOR,pch=20,cex=c(1.2),ncol=2)
+
+par(fig=c(0,0.8,0.55,1), new=TRUE)
+summary(lm(get(paste0("cluster_gini_",isotype))[,"cluster_gini"]~summary_data$sample))
+boxplot(get(paste0("cluster_gini_",isotype))[,"cluster_gini"]~summary_data$sample,
+        col=COLOR, horizontal=TRUE, axes=FALSE)
+
+par(fig=c(0.65,1,0,0.8),new=TRUE)
+summary(lm( get(paste0("cluster_gini_",isotype))[,"vertex_gini"]~summary_data$sample))
+boxplot(get(paste0("cluster_gini_",isotype))[,"vertex_gini"]~summary_data$sample,
+        col=COLOR,axes=FALSE)
+dev.off()
+
+
