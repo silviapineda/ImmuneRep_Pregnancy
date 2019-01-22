6b9f534260bd44d11dca54f7d625ad8acd354658
Brian Le <Brian.Le@ucsf.edu>
2019-01-22 13:19:50 -0800

    Update PrepareData_TCR.R
    corrected column name used for clone count from igh_clone_id to tcrb_clone_id

--- PrepareData_TCR.R
+++ PrepareData_TCR.R
@@ -62,10 +62,11 @@ data_qc$cdr3_seq <- gsub(" ","", data_qc$cdr3_seq_nt_q)
 #data_qc$isotype <- substr(data_qc$isosubtype, 1, 4)
 
 ##Count the somatic hypermutations
-data_qc$Vlength<-nchar(as.character(data_qc$v_sequence))
-v_sequence = as.character(data_qc$v_sequence)
-data_qc$SHM<-sapply(regmatches(v_sequence, gregexpr("[A-Z]", v_sequence, perl=TRUE)), length)
-data_qc$SHM_freq<-data_qc$SHM/data_qc$Vlength
+#SHM does not occur in t-cells
+#data_qc$Vlength<-nchar(as.character(data_qc$v_sequence))
+#v_sequence = as.character(data_qc$v_sequence)
+#data_qc$SHM<-sapply(regmatches(v_sequence, gregexpr("[A-Z]", v_sequence, perl=TRUE)), length)
+#data_qc$SHM_freq<-data_qc$SHM/data_qc$Vlength
 
 ##count the CDR3 length
 data_qc$CDR3_length<-nchar(as.character(data_qc$cdr3_seq))
@@ -73,39 +74,25 @@ data_qc$CDR3_length<-nchar(as.character(data_qc$cdr3_seq))
 ##count the read length
 data_qc$read_length<-nchar(as.character(data_qc$trimmed_sequence))
 
-##Read counts and clones per sample and data point
+##Read counts per sample (no isosubtypes in TCR)
 read_count <- table(data_qc$sample_label)
-#no isosubtypes in TCR data!
-#read_count_isotype <- table(data_qc$sample_label, data_qc$isotype)
-#colnames(read_count_isotype)[1] = "UNMAPPED"
-#reads <- cbind(read_count,read_count_isotype)
 reads <- cbind(read_count)
 
-
 ##Count number of clones per sample 
-data_qc$V_J_lenghCDR3_Clone_igh = paste(data_qc$v_gene, data_qc$j_gene, nchar(data_qc$cdr3_seq),data_qc$igh_clone_id,sep="_")
-read_count_ighClones<- unique(data_qc[,c("sample_label","V_J_lenghCDR3_Clone_igh")])
-clones_igh<-data.matrix(table(read_count_ighClones$sample_label))
-colnames(clones_igh)<-c("clones")
+data_qc$V_J_length_CDR3_Clone_tcrb = paste(data_qc$v_gene, data_qc$j_gene, nchar(data_qc$cdr3_seq), data_qc$tcrb_clone_id,sep="_")
+read_count_tcrbClones<- unique(data_qc[,c("sample_label","V_J_length_CDR3_Clone_tcrb")])
+clones_tcrb<-data.matrix(table(read_count_tcrbClones$sample_label))
+colnames(clones_tcrb)<-c("clones")
 
-reads_clones_igh<-cbind(reads,clones_igh)
-
-#####SHM
-sum_SHM_freq<-aggregate(data_qc$SHM_freq, by=list(data_qc$sample_label), FUN=sum)
-id<-match(rownames(reads_clones_igh),sum_SHM_freq$Group.1)
-colnames(sum_SHM_freq)[2]<-c("SHM")
-reads_clones_igh_SHM<-cbind(reads_clones_igh,sum_SHM_freq$SHM[id])
-#colnames(reads_clones_igh_SHM)[9]<-c("SHM")
-colnames(reads_clones_igh_SHM)[3]<-c("SHM")
+reads_clones_tcrb<-cbind(reads,clones_tcrb)
 
 ###length of CDR3
 cdr3_length<-aggregate(data_qc$CDR3_length,by=list(data_qc$sample_label), FUN=mean)
-id<-match(rownames(reads_clones_igh_SHM),cdr3_length$Group.1)
-reads_clones_igh_SHM_cdr3length<-cbind(reads_clones_igh_SHM,cdr3_length$x[id])
-#colnames(reads_clones_igh_SHM_cdr3length)[10]<-c("mean_CDR3_length")
-colnames(reads_clones_igh_SHM_cdr3length)[4]<-c("mean_CDR3_length")
+id<-match(rownames(reads_clones_tcrb),cdr3_length$Group.1)
+reads_clones_tcrb_cdr3length<-cbind(reads_clones_tcrb,cdr3_length$x[id])
+colnames(reads_clones_tcrb_cdr3length)[3]<-c("mean_CDR3_length")
 
-summary_data<-data.frame(reads_clones_igh_SHM_cdr3length)
+summary_data<-data.frame(reads_clones_tcrb_cdr3length)
 
 ###Introduce a variable to design mother and fetal
 #info_samples<-read.csv("Data/NormalSamplesScottBoydStanford9_13_18.csv")
@@ -122,7 +109,7 @@ simpson<-rep(NA,length(sample))
 for (i in 1:length(sample)){
   print(i)
   data_sample_unique<-data_qc[which(data_qc$sample_label==sample[i]),]
-  clones_sample<-data_sample_unique[,"V_J_lenghCDR3_Clone_igh"]
+  clones_sample<-data_sample_unique[,"V_J_length_CDR3_Clone_tcrb"]
   #To write file to run with Recon
   #write.delim(data.frame(table(table(clones_sample))),file=paste("clones_",sample[i],".txt",sep=""),sep="\t",col.names=F)
   fi<-as.numeric(table(clones_sample))/length(clones_sample)
