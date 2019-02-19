0632005023289c4c5cbb74c287c95e5396b1fcfe
silviapineda <silviapinedasanjuan@gmail.com>
2019-02-19 14:18:36 +0100

    First commit for convert plots

---
+++ ConvertPlot.sh
@@ -0,0 +1,49 @@
+
+##IGHA
+convert +append barplot_IGHA.tiff barplot_clones_IGHA.tiff IGHA1.tiff
+convert +append barplot_SHM_IGHA.tiff barplot_CDR3_length_IGHA.tiff IGHA2.tiff
+convert -append IGHA1.tiff IGHA2.tiff IGHA.tiff
+rm IGHA1.tiff
+rm IGHA2.tiff
+
+##IGHD
+convert +append barplot_IGHD.tiff barplot_clones_IGHD.tiff IGHD1.tiff
+convert +append barplot_SHM_IGHD.tiff barplot_CDR3_length_IGHD.tiff IGHD2.tiff
+convert -append IGHD1.tiff IGHD2.tiff IGHD.tiff
+rm IGHD1.tiff
+rm IGHD2.tiff
+
+##IGHG
+convert +append barplot_IGHG.tiff barplot_clones_IGHG.tiff IGHG1.tiff
+convert +append barplot_SHM_IGHG.tiff barplot_CDR3_length_IGHG.tiff IGHG2.tiff
+convert -append IGHG1.tiff IGHG2.tiff IGHG.tiff
+rm IGHG1.tiff
+rm IGHG2.tiff
+
+##IGHE
+convert +append barplot_IGHE.tiff barplot_clones_IGHE.tiff IGHE1.tiff
+convert +append barplot_SHM_IGHE.tiff barplot_CDR3_length_IGHE.tiff IGHE2.tiff
+convert -append IGHE1.tiff IGHE2.tiff IGHE.tiff
+rm IGHE1.tiff
+rm IGHE2.tiff
+
+##IGHM
+convert +append barplot_IGHM.tiff barplot_clones_IGHM.tiff IGHM1.tiff
+convert +append barplot_SHM_IGHM.tiff barplot_CDR3_length_IGHM.tiff IGHM2.tiff
+convert -append IGHM1.tiff IGHM2.tiff IGHM.tiff
+rm IGHM1.tiff
+rm IGHM2.tiff
+
+##Naive
+convert +append barplot_reads_naive.tiff barplot_clones_naive.tiff IGHM1.tiff
+convert +append barplot_SHM_naive.tiff barplot_CDR3_length_naive.tiff IGHM2.tiff
+convert -append IGHM1.tiff IGHM2.tiff IGHM_naive.tiff
+rm IGHM1.tiff
+rm IGHM2.tiff
+
+##Memory
+convert +append barplot_reads_memory.tiff barplot_clones_memory.tiff IGHM1.tiff
+convert +append barplot_SHM_memory.tiff barplot_CDR3_length_memory.tiff IGHM2.tiff
+convert -append IGHM1.tiff IGHM2.tiff IGHM_memory.tiff
+rm IGHM1.tiff
+rm IGHM2.tiff
